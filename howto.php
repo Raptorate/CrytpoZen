5b9da6c6d756f714d7dcb77e1b7d54a2475e13c7
Hodor-Fergus <75783235+Hodor-Fergus@users.noreply.github.com>
2021-05-28 02:01:25 +0200

    How To Help Feature

---
+++ howto.php
@@ -0,0 +1,66 @@
+<?php
+include('./includes/header.php');
+include('./includes/nav2.php');
+include('./includes/sidebar2.php');
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOW TO</title>
+</head>
+<body>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+    <!-- Top Header -->
+    <div class="jumbotron">
+        <div class="row mt-4">
+            <div class="col-12">
+                <div class="innerCntainer">
+                    <h1 class="headerText text-center">WELCOME TO OUR HOW TO PAGE</h1>
+                    <p class="headerText text-center">
+                        Here you learn how to buy and sell crypto.  
+                        How to deposit and withdraw cash and crypto. 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    
+    <br> <br>
+
+    
+<div class="headerText text-center">
+    <iframe width="800" height="400" src="https://www.youtube.com/embed/ss7mNhumlu0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;"></iframe>
+    <br><br>
+
+    <iframe width="800" height="400" src="https://www.youtube.com/embed/1newgMTWeWA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;"></iframe>
+    <br><br>
+
+    <iframe width="800" height="400" src="https://www.youtube.com/embed/1newgMTWeWA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;"></iframe>
+    <br><br>
+
+    <iframe width="800" height="400" src="https://www.youtube.com/embed/opnmfC0efKQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;"></iframe>
+    <br><br>
+
+    <iframe width="800" height="400" src="https://www.youtube.com/embed/aSJDEizhzGw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;"></iframe>
+    <br><br>
+
+    <iframe width="800" height="400" src="https://www.youtube.com/embed/UsP1hFVEkt8" title="YouTube video player" frameborder="0" allow="accelerometer; gyroscope;">BUY CRYTPO</iframe>
+    <br><br>
+
+    <iframe width="800" height="400" src="https://www.youtube.com/embed/ZJjzX6vUQsg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;"></iframe>
+    <br><br>
+</div>
+
+</body>
+</html>
+
+<?php
+include('./includes/footer.php');
+?>
